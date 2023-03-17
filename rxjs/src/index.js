135e84c842fc9735be04f406bd51d5dd2c53465d
rs-andrade <rodrigo.andrade@skyneti.com.br>
2023-03-17 18:02:07 +0000

    First Observable

--- rxjs/src/index.js
+++ rxjs/src/index.js
@@ -2,10 +2,21 @@ import {Observable} from 'rxjs';
 
 const observable = new Observable( (subscriber) => {
     subscriber.next('Hello Rodrigo')
+    subscriber.error('Error Rodrigo !!!')
+    subscriber.next('Hello for second')
+
+    subscriber.complete()
+    subscriber.next('Hello for third')
 })
 
 observable.subscribe({
     next: (value) => {
         console.log(value)
+    },
+    complete: () => {
+        console.log('complete hello called!!!')
+    },
+    error: (err) => {
+        console.error(err)
     }
 })
